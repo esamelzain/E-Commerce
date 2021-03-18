f5de958c05a5e4fa4d2b1c517377d222c17b9e9a
esam elzain <esam.a@websters.ae>
2021-03-18 17:52:19 +0400

    upload file problem

--- E-CommerceApi/Controllers/BrandsController.cs
+++ E-CommerceApi/Controllers/BrandsController.cs
@@ -2,11 +2,14 @@
 using E_CommerceApi.Models.vModels;
 using E_CommerceApi.Services;
 using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace E_CommerceApi.Controllers
@@ -15,9 +18,11 @@ namespace E_CommerceApi.Controllers
     [ApiController]
     public class BrandsController : ControllerBase
     {
+        private IHostingEnvironment _hostingEnvironment;
         private readonly BrandService BrandService;
-        public BrandsController(BrandService _BrandService)
+        public BrandsController(BrandService _BrandService, IHostingEnvironment hostingEnvironment)
         {
+            _hostingEnvironment = hostingEnvironment;
             BrandService = _BrandService;
         }
         [HttpPost]
@@ -26,10 +31,35 @@ namespace E_CommerceApi.Controllers
         {
             return await BrandService.GetAll();
         }
-        [HttpPost]
+        [HttpPost, DisableRequestSizeLimit]
         [Route("Add")]
         public async Task<BaseResponse> AddBrand([FromBody] Models.dbModels.Brand Brand)
         {
+            try
+            {
+                var file = Request.Form.Files[0];
+                string folderName = "Upload";
+                string webRootPath = _hostingEnvironment.WebRootPath;
+                string newPath = Path.Combine(webRootPath, folderName);
+                if (!Directory.Exists(newPath))
+                {
+                    Directory.CreateDirectory(newPath);
+                }
+                if (file.Length > 0)
+                {
+                    string fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    string fullPath = Path.Combine(newPath, fileName);
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
             return await BrandService.Add(Brand);
         }
         [HttpPost]
